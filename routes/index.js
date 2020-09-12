924e4ca9bd7da97af70f7cdfa54fee167a3341ee
selkasse <selkasse@gmu.edu>
2020-09-12 19:56:55 -0400

    styling navbar

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,10 @@
-
 const express = require('express');
 const router = express.Router();
 
-// Do work here
+// * Do work here
 router.get('/', (req, res) => {
-    res.send('Hey! It works!');
+    // res.send('Hey! It works!');
+    res.render('home', {title: 'Home'});
 });
 
 module.exports = router;
