43548e3fedb7220ab7680258aa6a87444e4961c0
selkasse <selkasse@gmu.edu>
2020-09-16 09:05:52 -0400

    add ability to edit account details

--- controllers/userController.js
+++ controllers/userController.js
@@ -45,3 +45,23 @@ exports.register = async (req, res, next) => {
     await register(user, req.body.password);
     next(); // * Pass to authController.login
 }
+
+exports.account = async (req, res) => {
+    res.render('account', {title: 'Edit your account'});
+}
+
+exports.updateAccount = async (req, res) => {
+    const updates = {
+        name: req.body.name,
+        email: req.body.email
+    };
+
+    const user = await User.findOneAndUpdate(
+        { _id: req.user._id },
+        { $set: updates },
+        { new: true, runValidators: true, context: 'query' }
+    );
+    req.flash('success', 'Your profile has been updated');
+    res.redirect('back');
+
+}
